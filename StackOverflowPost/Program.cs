8b420b587d02e85733b54a9811e5c9f7375fbe2a
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-03 15:26:53 -0500

    finished an online class exercise for creating a StackOverFlowPost

--- StackOverflowPost/Program.cs
+++ StackOverflowPost/Program.cs
@@ -6,7 +6,41 @@ namespace StackOverflowPost
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var post = new StackOverflowPost();
+
+            post.Title = "Hello, this is my first post!";
+            post.Description = "I am just practing C# fundemantals";
+            post.DateTimeCreated = DateTime.Now;
+
+            post.displayPost();
+
+            var repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine($@"
+Enter Y to up-vote the post
+Enter N to down-vote the post
+Enter View to view the post detail
+Enter other keys to kill this stupid app :)
+");
+                var userInput = Console.ReadLine().ToUpper();
+                if (userInput == "Y")
+                {
+                    post.UpVote();
+                }
+                else if (userInput == "N")
+                {
+                    post.DownVote();
+                }
+                else if (userInput == "VIEW")
+                {
+                    post.displayPost();
+
+                }
+                else {
+                    repeat = false;
+                }
+            }
         }
     }
 }
